3e23d0b8c38cc33ef7ee3cea20132b2b05b20580
Luiz Américo <meco.valandro@gmail.com>
2026-06-10 19:26:01 -0300

    refactor: refatorado estilos e pastas

--- app/Background.tsx
+++ app/Background.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
+import { BlurView } from "expo-blur";
+import { StyleSheet } from "react-native";
 
 interface BackgroundProps {
   children: React.ReactNode;
@@ -23,9 +25,15 @@ export default function Background({ children }: BackgroundProps) {
         }}
       >
         {/* Blobs de Fundo (Efeito Liquid Glass) */}
-        <View className="blob absolute top-[-15%] left-[-25%] w-[60vw] h-[60vw] bg-[#0078d4] rounded-full opacity-25" />
+        <View className="blob absolute top-[-15%] left-[-25%] w-[60vw] h-[60vw] bg-[#0078d4] rounded-full opacity-25"/>
         <View className="blob absolute bottom-[25%] right-[-25%] w-[40vw] h-[40vw] bg-[#00c2da] rounded-full opacity-10" />
         <View className="blob absolute bottom-[5%] left-[5%] w-[30vw] h-[30vw] bg-[#0078d4] rounded-full opacity-30" />
+
+        <BlurView 
+          intensity={100} // Equivale a força do blur (de 1 a 100)
+          tint="dark" // Cor do blur
+          style={StyleSheet.absoluteFill} // Faz o blur preencher todo o View pai
+        />
 
         {children}
       </ScrollView>
