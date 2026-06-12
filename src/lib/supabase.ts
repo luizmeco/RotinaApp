3ab86e18c6a4e938a6f29fc2b8e281f2761e8467
luizv <luizv@localhost.local>
2026-06-12 13:32:55 -0300

    fix: configurado storage para vercel

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -9,10 +9,24 @@ const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn("⚠️ Faltam as variáveis de ambiente do Supabase no arquivo .env");
 }
+const customStorage = {
+  getItem: async (key: string) => {
+    if (typeof window === 'undefined') return null;
+    return await AsyncStorage.getItem(key);
+  },
+  setItem: async (key: string, value: string) => {
+    if (typeof window === 'undefined') return;
+    await AsyncStorage.setItem(key, value);
+  },
+  removeItem: async (key: string) => {
+    if (typeof window === 'undefined') return;
+    await AsyncStorage.removeItem(key);
+  },
+};
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    storage: customStorage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false, // O RN não usa URLs de navegador para sessão
